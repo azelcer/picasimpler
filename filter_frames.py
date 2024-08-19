c073681d912933cc34c9406c9e83e04120597138
Andres Zelcer <45225345+azelcer@users.noreply.github.com>
2024-08-19 15:43:02 -0300

    Structure with functions

--- filter_frames.py
+++ filter_frames.py
@@ -1,26 +1,24 @@
 # -*- coding: utf-8 -*-
 """
 
-@author: aszalai
+@author: aszalai, azelcer
 """
 
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 from scipy.spatial import distance
 import h5py
-import h5py as h5
 import yaml
 import time
-from scipy.optimize import curve_fit
-from scipy.interpolate import interp1d
 from tqdm import tqdm
+import logging as _lgn
 
+_lgn.basicConfig()
+_lgr = _lgn.getLogger(__name__)
 
-filename = "/mnt/fs_public/Andi/Para Andy/SF2_GFP_F3_532_2mW_20pM_25EmGain_100ms_concat_aligned.hdf5"
+filename = "/home/azelcer/Dropbox/2024/simpler/example_spectrin_large.hdf5"
 
-
-#%% simpler parameters
 
 # angle = 67.5
 # alpha = 0.9
@@ -32,132 +30,55 @@ filename = "/mnt/fs_public/Andi/Para Andy/SF2_GFP_F3_532_2mW_20pM_25EmGain_100ms
 # NA = 1.42
 
 
-px_size = 133 # nm
-radius_threshold = 75 # nm
-
-#%% load files
-
-with pd.HDFStore(filename,'r') as store:
-    data=store['locs']
-
-#with h5py.File(filename, "r") as store:
-#    data=store['locs']
-
-#### read yaml file and store data in info
-yaml_file = filename[:-5] + ".yaml"
-with open(yaml_file, "r") as info_file:
-    info = list(yaml.load_all(info_file, Loader=yaml.FullLoader)) 
-#### save data of info
-
-
-
-#%%  filter frames and perform SIMPLER
-
-radius_threshold_px = radius_threshold/px_size
-# start = time.time()
-
-# frames = np.array(data['frame'])
-# x = np.array(data['x'])
-# y = np.array(data['y'])
-# xy = np.column_stack((np.transpose(x),np.transpose(y)))
-# discard_yn_orig = np.ones((len(x),))
-
-# for i in tqdm(range(len(x))):
-#     if frames[i]>0 and frames[i]<np.max(frames):
-#         idx_frame_minus_1 = np.where(frames == (frames[i]-1))
-#         idx_frame_plus_1 = np.where(frames == (frames[i]+1))
-#         if len(idx_frame_minus_1[0]) > 0 and len(idx_frame_plus_1[0]) > 0:
-#             distance_frame_minus_1 = distance.cdist(np.reshape(xy[i,:],(1,2)), 
-#                                                     xy[idx_frame_minus_1[0],:], 'euclidean')
-#             distance_frame_minus_1 = np.sort(distance_frame_minus_1)
-#             distance_frame_plus_1 = distance.cdist(np.reshape(xy[i,:],(1,2)), 
-#                                                    xy[idx_frame_plus_1[0],:], 'euclidean')
-#             distance_frame_plus_1 = np.sort(distance_frame_plus_1)
-#             if distance_frame_minus_1[0,0] < radius_threshold_px and distance_frame_plus_1[0,0] < radius_threshold_px:
-#                 discard_yn_orig[i] = 0
-
-# end1 = time.time()
-# print('Time of filtering step original (s):', end1-start)
-
-r_th_sq = (radius_threshold/px_size)**2
-start = time.time()
-
-frames = np.array(data['frame'])
-x = np.array(data['x'])
-y = np.array(data['y'])
-xy = np.column_stack((np.transpose(x), np.transpose(y)))
-discard_yn = np.ones((len(x),))
-framejump = np.nonzero(np.diff(frames, prepend=-np.inf, append=np.inf) != 0)[0]
-distance_next = None
-for idxframe in tqdm(range(1, len(framejump)-2)):
-    prevframe = frames[framejump[idxframe-1]]
-    nextframe = frames[framejump[idxframe+1]]
-    frame = frames[framejump[idxframe]]
-    if (frame + 1 != nextframe):
-        distance_next = None
-        continue
-    if (frame - 1 != prevframe):
-        continue
-    f_slice = slice(framejump[idxframe], framejump[idxframe+1])
-    prev_slice = slice(framejump[idxframe-1], framejump[idxframe])
-    next_slice = slice(framejump[idxframe+1], framejump[idxframe+2])
-    if distance_next is not None:
-        distance_prev = distance_next.T
-    else:
-        distance_prev = distance.cdist(xy[f_slice], xy[prev_slice], 'sqeuclidean')
-    distance_next = distance.cdist(xy[f_slice], xy[next_slice], 'sqeuclidean')
-    has_prev = np.any(distance_prev < r_th_sq, axis=1)
-    has_next = np.any(distance_next < r_th_sq, axis=1)
-    discard_yn[f_slice][np.logical_and(has_prev, has_next)] = 0
+def filter_data(data, radius_threshold, px_size) -> np.ndarray:
+    """Filter localizations according to SIMPLER criteria."""
+    r_th_sq = (radius_threshold/px_size)**2
+    start = time.time()
+    frames = np.array(data['frame'])
+    xy = np.column_stack((np.transpose(data['x']), np.transpose(data['y'])))
+    discard_yn = np.ones((len(xy),), dtype=np.uint64)
+    framejump = np.nonzero(np.diff(frames, prepend=-np.inf, append=np.inf) != 0)[0]
+    distance_next = None
+    for idxframe in tqdm(range(1, len(framejump)-2)):
+        prevframe = frames[framejump[idxframe-1]]
+        nextframe = frames[framejump[idxframe+1]]
+        frame = frames[framejump[idxframe]]
+        if (frame + 1 != nextframe):
+            distance_next = None
+            continue
+        if (frame - 1 != prevframe):
+            continue
+        f_slice = slice(framejump[idxframe], framejump[idxframe+1])
+        prev_slice = slice(framejump[idxframe-1], framejump[idxframe])
+        next_slice = slice(framejump[idxframe+1], framejump[idxframe+2])
+        if distance_next is not None:
+            distance_prev = distance_next.T
+        else:
+            distance_prev = distance.cdist(xy[f_slice], xy[prev_slice], 'sqeuclidean')
+        distance_next = distance.cdist(xy[f_slice], xy[next_slice], 'sqeuclidean')
+        has_prev = np.any(distance_prev < r_th_sq, axis=1)
+        has_next = np.any(distance_next < r_th_sq, axis=1)
+        discard_yn[f_slice][np.logical_and(has_prev, has_next)] = 0
+    end = time.time()
+    print('Time of filtering step nuevo (s):', end-start)
+    idx_to_discard = np.where(discard_yn == 1)
+    return idx_to_discard
 
 
+def calculate_z(data, alpha, df, N0) -> np.ndarray:
+    """Calculate z according to SIMPLER criteria.
 
-end1 = time.time()
-print('Time of filtering step nuevo (s):', end1-start)
-
-
-start = time.time()
-
-frames = np.array(data['frame'])
-x = np.array(data['x'])
-y = np.array(data['y'])
-xy = np.column_stack((np.transpose(x),np.transpose(y)))
-discard_ynbis = np.ones((len(x),))
-
-framejump = np.nonzero(np.diff(frames,prepend=-np.inf, append=np.inf)!=0)[0]
-for idxframe in tqdm(range(1, len(framejump)-2)):
-    prevframe = frames[framejump[idxframe-1]]
-    nextframe = frames[framejump[idxframe+1]]
-    frame = frames[framejump[idxframe]]
-    if frame - 1 == prevframe:
-        idx_frame_minus_1 = np.arange(framejump[idxframe-1], framejump[idxframe])
-    else:
-        idx_frame_minus_1 = np.empty((0,))
-    if frame + 1 == nextframe:
-        idx_frame_plus_1 = np.arange(framejump[idxframe+1], framejump[idxframe+2])
-    else:
-        idx_frame_plus_1 = np.empty((0,))
-    if len(idx_frame_minus_1) > 0 and len(idx_frame_plus_1) > 0:
-        for i in range(framejump[idxframe], framejump[idxframe+1]):
-            distance_frame_minus_1 = distance.cdist(np.reshape(xy[i,:],(1,2)), xy[idx_frame_minus_1,:], 'euclidean')
-            distance_frame_minus_1 = np.sort(distance_frame_minus_1)
-            distance_frame_plus_1 = distance.cdist(np.reshape(xy[i,:],(1,2)), xy[idx_frame_plus_1,:], 'euclidean')
-            distance_frame_plus_1 = np.sort(distance_frame_plus_1)
-            if distance_frame_minus_1[0,0] < radius_threshold_px and distance_frame_plus_1[0,0] < radius_threshold_px:
-                discard_ynbis[i] = 0
-
-end1 = time.time()
-print('Time of filtering step bis (s):', end1-start)
-
-print(np.all(discard_ynbis == discard_yn))
-# print(np.all(discard_yn_orig == discard_yn))
-
-exit(1)
-idx_to_discard = np.where(discard_yn == 1)
-
-data_filtered = data.drop(labels=idx_to_discard[0], axis=0)
-data_filtered = data_filtered.reset_index(level=None, drop=True, inplace=False,
-                                          col_level=0)
+    We use fixed N0 for now.
+    """
+    # N0 = _calculate_N0(data, params)
+    max_photons = np.max(data['photons'])
+    min_alpha = 1 - (np.min(data['photons']) / max_photons)
+    if alpha < min_alpha:
+        _lgr.warning("Some location intensities are below non-evanescent excitation expected intensity")
+    if N0 < max_photons:
+        _lgr.warning("Some location intensities are above z=0 intensity")
+    rv = df * np.log(alpha/(data['photons']/N0 - (1 - alpha)))
+    return rv
 
 
 def df_to_sarray(df):
@@ -183,7 +104,6 @@ def df_to_sarray(df):
         except:
             print(col.name, col_type, col_type.type, type(col))
             raise
-
     v = df.values
     types = df.dtypes
     numpy_struct_types = [make_col_type(types[col], df.loc[:, col]) for col in df.columns]
@@ -203,17 +123,32 @@ def df_to_sarray(df):
     return z, dtype
 
 
-sa, saType = df_to_sarray(data_filtered)
-# Open/create the HDF5 file
-f = h5py.File(filename[:-5]+'_frames_filtered.hdf5', 'a')
-# Save the structured array
-f.create_dataset('locs', data=sa, dtype=saType)
-f.close()
+if __name__ == '__main__':
+    start = time.time()
+    with pd.HDFStore(filename, 'r') as store:
+        data = store['locs']
 
-# info[0]['Frames'] = np.max(data_filtered['frame'])
-
-with open(filename[:-5]+'_frames_filtered.yaml', "w") as file:
-    yaml.dump_all(info, file, default_flow_style=False)
-
-end2 = time.time()
-print('Script total time (s):', end2-start)
+    yaml_file = filename[:-5] + ".yaml"
+    with open(yaml_file, "r") as info_file:
+        info = list(yaml.load_all(info_file, Loader=yaml.FullLoader))
+    # px_size = 133 # nm
+    px_size = info[1]['Pixelsize']
+    radius_threshold = 75  # nm
+    idx_to_discard = filter_data(data, radius_threshold, px_size)
+    data_filtered = data.drop(labels=idx_to_discard[0], axis=0)
+    data_filtered = data_filtered.reset_index(level=None, drop=True, inplace=False,
+                                              col_level=0)
+    print("minimum alpha is:", 1 - (np.min(data['photons']) / np.max(data['photons'])))
+    z = calculate_z(data_filtered, 0.95, 100, np.max(data['photons']))
+    data_filtered['z'] = z
+    # save data of info
+    sa, saType = df_to_sarray(data_filtered)
+    # Open/create the HDF5 file
+    f = h5py.File(filename[:-5]+'_frames_filtered.hdf5', 'a')
+    # Save the structured array
+    f.create_dataset('locs', data=sa, dtype=saType)
+    f.close()
+    with open(filename[:-5]+'_frames_filtered.yaml', "w") as file:
+        yaml.dump_all(info, file, default_flow_style=False)
+    end = time.time()
+    print('Script total time (s):', end - start)

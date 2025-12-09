1c035c3d72185c38c25308f13c7ed87426b30f78
Andres Zelcer <45225345+azelcer@users.noreply.github.com>
2025-12-08 23:53:41 -0300

    Advance, try matplotlib

--- frontend_simpler.py
+++ frontend_simpler.py
@@ -5,7 +5,7 @@ import numpy as _np
 import pathlib as _pathlib
 from PyQt5.QtCore import pyqtSignal, pyqtSlot, Qt, QAbstractTableModel
 from PyQt5.QtWidgets import (
-    QGroupBox,
+    # QGroupBox,
     QMainWindow,
     QAction,
     QLabel,
@@ -13,8 +13,8 @@ from PyQt5.QtWidgets import (
     QCheckBox,
     QHBoxLayout,
     QVBoxLayout,
-    QFormLayout,
-    QLineEdit,
+    # QFormLayout,
+    # QLineEdit,
     QSpinBox,
     QDoubleSpinBox,
     QMessageBox,
@@ -26,7 +26,10 @@ from PyQt5.QtWidgets import (
     QTableView,
 )
 from PyQt5 import QtGui as _QtGui
-import pyqtgraph as _pg
+# import pyqtgraph as _pg
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas # or backend_qt6agg
+from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar # or backend_qt6agg
 import logging as _lgn
 from simpler_tools import SimplerAnalysisParameters, SIMPLERData
 
@@ -86,36 +89,40 @@ def create_labeled_int(name: str, external_layout: QBoxLayout,
 
 
 class SIMPLERTableModel(QAbstractTableModel):
-    def __init__(self, data):
+    def __init__(self, data: SIMPLERData):
         super().__init__()
         self._data = data
+        self._columns = data.get_column_names()
 
     def data(self, index, role):
         if role == Qt.DisplayRole:
-            # See below for the nested-list data structure.
-            # .row() indexes into the outer list,
-            # .column() indexes into the sub-list
-            return str(self._data.data.iloc[index.row(), index.column()])
+            # suponemos siempre numpy
+            val = self._data.data[index.row()][index.column()]
+            return "-" if _np.isnan(val) else str(val)
+        elif role == Qt.BackgroundRole:
+            if not self._data.data[index.row()]["valid"]:
+                return _QtGui.QBrush(_QtGui.QColor(0xc0c0c0))
 
     def rowCount(self, index):
         return self._data.data.shape[0]
 
     def columnCount(self, index):
-        return self._data.data.shape[1]
+        return len(self._columns)
 
     def headerData(self, section: int, orientation: Qt.Orientation, role: Qt.DisplayRole):
         if role == Qt.DisplayRole:
             if orientation == Qt.Orientation.Vertical:
                 return str(section)
-            return str(self._data.data.columns[section])
+            return str(self._columns[section])
 
 
 class SimplerWidget(QFrame):
 
     apply_signal = pyqtSignal(SimplerAnalysisParameters)
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self, parent: QWidget, *args, **kwargs):
+        super().__init__(parent=parent, *args, **kwargs)
+        self._parent = parent
         self._init_GUI()
 
     def freeze(self):
@@ -130,7 +137,10 @@ class SimplerWidget(QFrame):
         self._alpha_sb = create_labeled_float("\u03B1<sub>F</sub>", layout, 10, 1, 1)
         self._dF_sb = create_labeled_float("d<sub>F</sub> / nm", layout, 10, 1, 1)
         self._N0_sb = create_labeled_int("N<sub>0</sub>", layout, 10000,)
+        self._filter_button = QPushButton("Filter", self)
+        self._filter_button.pressed.connect(self._parent.filter_data)
         self._apply_button = QPushButton("Apply", self)
+        layout.addWidget(self._filter_button)
         layout.addWidget(self._apply_button)
         self.setLayout(layout)
 
@@ -202,8 +212,19 @@ class DataPlotWidget(QFrame):
 
     def _init_GUI(self):
         layout = QHBoxLayout()
-        self._plot = _pg.PlotWidget()
-        layout.addWidget(self._plot)
+        plt_lyt = QVBoxLayout()
+        # self._plot = _pg.PlotWidget()
+        # self._plot.setAspectLocked(1)
+        self.fig = Figure(figsize=(5, 4), dpi=100)
+        self.ax = self.fig.add_subplot(111)  # Add a subplot to the figure
+        self.ax.axis("scaled")
+        # self.sp = self.ax.scatter([], [])
+        self._plot = FigureCanvas(self.fig)
+        toolbar = NavigationToolbar(self._plot, self)
+        plt_lyt.addWidget(toolbar)
+        plt_lyt.addWidget(self._plot)
+        layout.addLayout(plt_lyt, stretch=3)
+        # layout.addWidget(self._plot)
 
         chk_layout = QVBoxLayout()
         self._unfiltered_chk = QCheckBox("Not included")
@@ -223,8 +244,25 @@ class DataPlotWidget(QFrame):
         x = data["x"]
         y = data["y"]
         # Meter transformacion a µm
-        p = self._plot.plot(x, y, pen=None, symbol="o", symbolSize=.20, pxMode=True) # default True == son pixeles
-        # p.setDownsampling(ds=100, auto=True, method="peak")
+        # self._scatter_plot = self._plot.plot(x, y, pen=None, symbolpen=None, symbol="s", symbolSize=.2, pxMode=False) # default True == son pixeles
+        # p.setDownsampling(ds=None, auto=True, method="subsample")
+        # p.setClipToView(True)
+        # p.disableAutoRange()
+        self.ax.clear()
+        self.ax.scatter(x, y)
+
+    def update_data(self, new_data: SIMPLERData):
+        # borrar todo
+        # actualizar
+        # FIXME: cambiar este acceso feo, es sólo para arrancar a dibujar
+        data = new_data.data
+        x = data["x"][data["valid"]]
+        y = data["y"][data["valid"]]
+        # Meter transformacion a µm
+        # self._scatter_plot.setData(x, y)
+        self.ax.clear()
+        self.ax.scatter(x, y)
+        # self._scatter_plot.draw()
 
 
 class Frontend(QMainWindow):
@@ -234,6 +272,7 @@ class Frontend(QMainWindow):
     """
 
     _modified = False
+    _data = None
 
     def __init__(self, *args, **kwargs):
         """Init Frontend."""
@@ -252,9 +291,14 @@ class Frontend(QMainWindow):
         open_act.setShortcut('Ctrl+O')
         open_act.setStatusTip('Open file')
         open_act.triggered.connect(self.file_open)
+        save_act = QAction('&Save', self)
+        save_act.setShortcut('Ctrl+S')
+        save_act.setStatusTip('Save file')
+        save_act.triggered.connect(self.file_save)
         self._menu_bar = mb = self.menuBar()
         fileMenu = mb.addMenu('&File')
         fileMenu.addAction(open_act)
+        fileMenu.addAction(save_act)
 
     def _setup_gui(self):
         """Create and lay out all GUI objects."""
@@ -276,6 +320,23 @@ class Frontend(QMainWindow):
     def notify(self, msg: str):
         """Convenience function."""
         self._status_bar.showMessage(str)
+
+    def file_save(self):
+        """Checks and opens a file."""
+        if not self._modified:
+            QMessageBox.information(
+                self, 'Message', "Noting to save",
+                QMessageBox.Ok, QMessageBox.Ok,
+                )
+            return
+        print("not implemented")
+
+    def filter_data(self):
+        if self._data is None:
+            _lgr.info("No data to filter")
+            return
+        self._data.filter_data(self._SIMPLER_widget.get_analysis_parameters())
+        self._plot_widget.update_data(self._data)
 
     def file_open(self):
         """Checks and opens a file."""
@@ -332,8 +393,8 @@ if __name__ == '__main__':
     app.setStyle('Windows')
     gui = Frontend()
     gui.show()
-    gui.raise_()
     gui.activateWindow()
+    gui.raise_()
 
     app.exec_()
     app.quit()
